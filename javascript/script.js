4736425edec72f757f1e025085b75adb88cbcb57
NoaBrecht <99073951+NoaBrecht@users.noreply.github.com>
2023-11-23 21:14:36 +0100

    Add product to shopping cart functionality
    TEST!

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+function addProductToWinkelmandje() {
+    // Create an "li" node:
+    const node = document.createElement("aside");
+
+
+    function addProductToWinkelmandje() {
+        // Create an "article" node:
+        const node = document.createElement("article");
+        node.className = "card";
+        node.innerHTML = `
+                    <h1>SPECTRAFROST Swiftlite Ultrabook</h1>
+                    <img src="assets/images/SpectraFrost Ultrabook.jpeg" alt="SpectraFrost Ultrabook" />
+
+                    <p class="price">Starting at &euro;1259</p>
+                    <p>
+                        Maak kennis met de SPECTRAFROST Swiftlite Ultrabook - waar
+                        betaalbaarheid samengaat met draagbaarheid.
+                    </p>
+                    <button>Add to Cart</button>
+            `;
+
+        // Append the "article" node to the body:
+        const elBody = document.getElementsByTagName("body")[0];
+        elBody.appendChild(node);
+        console.log('test');
+    }
+}
+
+let elWinkelmandje = document.getElementById('winkelmandje');
+
+elWinkelmandje.addEventListener('blur', addProductToWinkelmandje, false);
+// 'blur' is één van de vele DOM events die wordt aangeroepen
