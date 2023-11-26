b8c36597f6ed0667276c2f0187aeca6dca92c3e8
NoaBrecht <99073951+NoaBrecht@users.noreply.github.com>
2023-11-26 15:57:25 +0100

    Refactor product array to use JSON format

--- javascript/script.js
+++ javascript/script.js
@@ -22,60 +22,41 @@ function addToWishList() {
 
 //* Winkelmandje
 
-// make a 2d array with all the products and prices
-let products = [
-    ["LuminX 32-inch White Gaming Monitor", 1480, "assets/images/LuminX.jpeg"],
-    ["SPECTRAFROST X1 GAMING LAPTOP ", 4999],
-    ["SPECTRAFROST Swiftlite Ultrabook", 1259],
-    ["SPECTRAFROST Swiftlite Ultrabook", 1259],
-    ["SPECTRAFROST Swiftlite Ultrabook", 1259],
-    ["SPECTRAFROST Swiftlite Ultrabook", 1259],
-    ["SPECTRAFROST Swiftlite Ultrabook", 1259],
-    ["SPECTRAFROST Swiftlite Ultrabook", 1259],
-    ["SPECTRAFROST Swiftlite Ultrabook", 1259],
-];
-
-// make a function to add the products to the cart
-function addProductToWinkelmandje(productIndex) {
-    product = products[productIndex];
-    productName = product[0];
-    productPrice = product[1];
-    productImage = product[2];
-    // create the asied element
-    const aside = document.createElement("aside");
-    const product = document.createElement("article");
-    product.className = "card";
-    product.innerHTML = `
-                    <h1>${productName}</h1>
-                    <img src="${productImage}" alt="${productName}" />
-
-                    <p class="price">${productPrice}</p>
-            `;
-    const elBody = document.getElementsByTagName("body")[0];
-    elBody.appendChild(aside);
-}
-
-
-// function addProductToWinkelmandje() {
-//     // Create an "li" node:
-//     const aside = document.createElement("aside");
-//     // Create an "article" node:
-//     const product = document.createElement("article");
-//     node.className = "card";
-//     node.innerHTML = `
-//                     <h1>SPECTRAFROST Swiftlite Ultrabook</h1>
-//                     <img src="assets/images/SpectraFrost Ultrabook.jpeg" alt="SpectraFrost Ultrabook" />
-
-//                     <p class="price">Starting at &euro;1259</p>
-//                     <p>
-//                         Maak kennis met de SPECTRAFROST Swiftlite Ultrabook - waar
-//                         betaalbaarheid samengaat met draagbaarheid.
-//                     </p>
-//                     <button>Add to Cart</button>
-//             `;
-
-//     // Append the "article" node to the body:
-//     const elBody = document.getElementsByTagName("body")[0];
-//     elBody.appendChild(aside);
-//     console.log('test');
-// }
+// Define a JSON array of products
+const products = [
+    {
+        "name": "SPECTRAFROST Swiftlite Ultrabook",
+        "price": "1259",
+        "image": "assets/images/SpectraFrost Ultrabook.jpeg"
+    },
+    {
+        "name": "SPECTRAFROST EcoWoodElite Desktop PC",
+        "price": "5469",
+        "image": "assets/images/EcoWood.jpeg"
+    },
+    {
+        "name": "LuminX 32-inch White Gaming Monitor",
+        "price": "1480",
+        "image": "assets/images/LuminX.jpeg"
+    },
+    {
+        "name": "SpectraFrost X1 Gaming Laptop",
+        "price": "1480",
+        "image": "assets/images/LuminX.jpeg"
+    },
+    {
+        "name": "LuminX 32-inch White Gaming Monitor",
+        "price": "1480",
+        "image": "assets/images/LuminX.jpeg"
+    },
+    {
+        "name": "LuminX 32-inch White Gaming Monitor",
+        "price": "1480",
+        "image": "assets/images/LuminX.jpeg"
+    },
+    {
+        "name": "LuminX 32-inch White Gaming Monitor",
+        "price": "1480",
+        "image": "assets/images/LuminX.jpeg"
+    }
+]
